6928169adfbd45e49b85eefc291f7721c50fd5c9
Anshuman2322 <anshuman2322singh@gmail.com>
2025-11-06 02:54:15 +0530

    feat: Conflict Solve

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import ShopByCategory from './pages/ShopByCategory';
-import "./App.css";
+import Hero from "./components/Hero";
+import CategorySection from "./components/CategorySection";
+import FeaturedMedicines from "./components/FeaturedMedicines";
+import Footer from "./components/Footer";
 import ShopByCategory from "./pages/ShopByCategory";
+import MedicineDetails from "./pages/MedicineDetails";
+import About from "./pages/About";
+import "./App.css";
 
+{/Temporary comment/}
 function App() {
   return (
     <div className="h-full w-full bg-white">
       <Navbar />
-      <ShopByCategory />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Hero />
+              <CategorySection />
+              <FeaturedMedicines />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/shop" element={<ShopByCategory />} />
+        <Route path="/products/:id" element={<MedicineDetails />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
